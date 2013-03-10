df0b9232faf8d6fc6f8ad7e06364dfda78bc70fe
Pavel Kalian <pavel@kalian.cz>
2013-03-10 10:59:47 -0600

    version bump to 0.96

--- src/aisradar_pi.h
+++ src/aisradar_pi.h
@@ -36,7 +36,7 @@
 #endif //precompiled headers
 
 #define     PLUGIN_VERSION_MAJOR    0
-#define     PLUGIN_VERSION_MINOR    95
+#define     PLUGIN_VERSION_MINOR    96
 
 #define     MY_API_VERSION_MAJOR    1
 #define     MY_API_VERSION_MINOR    7
