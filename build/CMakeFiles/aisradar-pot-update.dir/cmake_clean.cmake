5d62a3bdb96090377adf6a26e62b0995095c464c
Johan van der Sman <johan.sman@gmail.com>
2021-04-01 11:50:14 +0200

    removed build directory

--- build/CMakeFiles/aisradar-pot-update.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/aisradar-pot-update"
-  "po/aisradar_pi.pot.dummy"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/aisradar-pot-update.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
